c6a7a5c8a703c9308d94271741468b4239a51aaf
LAPTOP-6637MLQG\ADMIN <thanh.lv10041999@gmail.com>
2026-07-23 01:37:37 +0700

    commit

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   const isGitHubPages = location.hostname.endsWith("github.io");
   console.log("isGitHubPages:", isGitHubPages, "hostname:", location.hostname, "pathname:", location.pathname);
   const basePath = isGitHubPages ? location.hostname +"/HRPMS/" : document.body.dataset.basePath;
+  const basePath1 = document.body.dataset.basePath;
   console.log("basePath:", basePath);
   //const basePath = document.body.dataset.basePath || "";
   const currentUser = getCurrentUser();
@@ -26,7 +27,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   }
 
   try {
-    const response = await fetch(`${basePath}pages/components/sidebar.html`, {
+    const response = await fetch(`${basePath1}pages/components/sidebar.html`, {
       cache: "no-store",
     });
 
