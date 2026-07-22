a269603767c11f70d90dd0132be2c7ea183de786
LAPTOP-6637MLQG\ADMIN <thanh.lv10041999@gmail.com>
2026-07-22 21:55:23 +0700

    commit

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -4,7 +4,7 @@ import { getAuthorContext, applyRoleGuards } from "./common/author.js";
 document.addEventListener("DOMContentLoaded", async () => {
   const isGitHubPages = location.hostname.endsWith("github.io");
   console.log("isGitHubPages:", isGitHubPages, "hostname:", location.hostname, "pathname:", location.pathname);
-  const basePath = isGitHubPages ? document.body.dataset.basePath +"HRPMS/" : document.body.dataset.basePath;
+  const basePath = isGitHubPages ? location.hostname +"/HRPMS/" : document.body.dataset.basePath;
   console.log("basePath:", basePath);
   //const basePath = document.body.dataset.basePath || "";
   const currentUser = getCurrentUser();
